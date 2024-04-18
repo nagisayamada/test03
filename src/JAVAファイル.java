5163d58500abd8b7b4d772f2812c64d126b5fea6
nagisayamada <nagisa0914@gmail>
2024-04-18 21:49:15 +0900

    java2

--- src/JAVAファイル.java
+++ src/JAVAファイル.java
@@ -2,7 +2,16 @@
 public class JAVAファイル {
 
 	public static void main(String[] args) {
-		int a = 6 % 3;
-		System.out.println(a);
+		String a = "青";
+		switch(a) {
+		case "赤":
+			System.out.println("赤組です");
+			break;
+		case "白":
+			System.out.println("白組です");
+			break;
+		default:
+			System.out.println("エラーです");
 		}
+	}
 }
