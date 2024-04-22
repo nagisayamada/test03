d8844a14fa2d5366584b8ada6ef628dbbbad0373
nagisayamada <nagisa0914@gmail>
2024-04-22 20:29:50 +0900

    java3

---
+++ TestProject01/src/JAVAファイル.java
@@ -0,0 +1,14 @@
+
+public class JAVAファイル {
+	
+	public static void main(String[] args) {
+		public static string hello(){
+			return "こんにちは";
+			System.out.println("山田さん。"+hello());
+			System.out.println("<br>");
+			System.out.println("鈴木さん。"+hello());
+			System.out.println("<br>");
+			System.out.println("佐藤さん。"+hello());
+		}
+	}
+}
